ee173a677be7741d186f80613044948f250bddae
mobius <mobius@Dock.home>
2017-12-10 23:29:07 +0100

    script typo

--- launch_youtube-dl.sh
+++ launch_youtube-dl.sh
@@ -3,9 +3,9 @@
 
 if [ -z $1 ]; then
     echo "No URL given :: exiting"
-elif [ -d $1 ] ; then
+else
     workspace=$(pwd)
-    echo "RUNNING : docker run -v $workspace:/home/ -it vincentravera/youtube-dl-docker youtube-dl  --extract-audio
+    echo "RUNNING : docker run -v $workspace:/mnt -it vincentravera/youtube-dl-docker youtube-dl  --extract-audio
     --audio-format mp3 -o \"%(title)s.%(ext)s\" $1"
-    docker run -v $workspace:/home/ -it vincentravera/youtube-dl-docker youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $1
+    docker run -v $workspace:/mnt -it vincentravera/youtube-dl-docker youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $1
 fi
