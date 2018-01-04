9ed72b6ee1faf12e35a64ad3693593fc1dd0b760
mobius <mobius@Dock.home>
2018-01-04 19:47:15 +0100

    Errors no longer stops the download

--- launch_youtube-dl.sh
+++ launch_youtube-dl.sh
@@ -5,7 +5,6 @@ if [ -z $1 ]; then
     echo "No URL given :: exiting"
 else
     workspace=$(pwd)
-    echo "RUNNING : docker run -v $workspace:/mnt -it vincentravera/youtube-dl-docker youtube-dl  --extract-audio
-    --audio-format mp3 -o \"%(title)s.%(ext)s\" $1"
-    docker run -v $workspace:/mnt -it vincentravera/youtube-dl-docker youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $1
+    echo "RUNNING : docker run -v $workspace:/mnt -it vincentravera/youtube-dl-docker youtube-dl --continue --ignore-errors --no-overwrites  --extract-audio --audio-format mp3 -o \"%(title)s.%(ext)s\" $1"
+    docker run -v $workspace:/mnt -it vincentravera/youtube-dl-docker youtube-dl --continue --ignore-errors --no-overwrites --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" $1
 fi
